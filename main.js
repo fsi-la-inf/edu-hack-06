c8c34131b6996a667fd615a76b7180f5364d208e
KDan213 <kd18@web.de>
2024-08-06 16:10:23 +0200

    eingabe Funktioniert

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
 	var numberInput = [-1, 'x+', 20];
-	var numberOutput = [0, 'x+', -7]
+	var numberOutput = [0, 'x+', -7];
+
 	var markedInput = Array(numberInput.length).fill(false); // Array to keep track of marked buttons
 	var markedOutput = Array(numberOutput.length).fill(false);
 
 	const buttonContainerInput = document.getElementById("button-container");
 	const buttonContainerOutput = document.getElementById("button-containerOutput");
 	const makeButton = document.getElementById("make");
-
-
+	const mI = document.getElementById("mI");
+	const tI = document.getElementById("tI");
+	const mO = document.getElementById("mO");
+	const tO = document.getElementById("tO");
+	const submitButton = document.getElementById("submit-button");
+	submitButton.addEventListener("click", () => {
+		numberInput[0] = parseInt(mI.value);
+		numberInput[2] = parseInt(tI.value);
+		numberOutput[0] = parseInt(mO.value);
+		numberOutput[2] = parseInt(tO.value);
+		console.log(numberInput);
+		updateButtonArray();
+	});
 
 	// Function to update the array of buttons
 	function updateButtonArray() {
--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const inputElement = document.getElementById("number-input");
+    const mI = document.getElementById("mI");
+    const tI = document.getElementById("tI");
+    const mO = document.getElementById("mO");
+    const tO = document.getElementById("tO");
     const submitButton = document.getElementById("submit-button");
     let number;
-
+    var numberInput = [-1, 'x+', 20];
+    var numberOutput = [0, 'x+', -7];
     submitButton.addEventListener("click", () => {
-        const inputValue = inputElement.value;
-        number = parseFloat(inputValue);
-        console.log(number);
-        if (!isNaN(number)) {
-            console.log("Number stored in variable:", number);
-        } else {
-            console.log("Please enter a valid number.");
-        }
+        numberInput[0] = parseInt(mI.value);
+        numberInput[2] = parseInt(tI.value);
+        numberOutput[0] = parseInt(mO.value);
+        numberOutput[2] = parseInt(tI.value);
+        console.log(numberInput)
     });
 });
